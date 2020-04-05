8d27e0e3e2bd3fe8066e44597f98d2031423fd36
Rjanczak <jaraf2@gmail.com>
2020-04-05 21:34:00 +0200

    Add files via upload
    Initial commit

---
+++ Heart event minigames/BoardGameScenario.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace ReplaySolarionChronicles
+{
+    public class BoardGameScenario
+    {
+        private string folderPath;
+        public string Name { get; set; }
+        
+        public List<BoardGameScene> Scenes { get; set; }
+
+        public static BoardGameScenario LoadFromJSON(string fileName)
+        {
+            string fileContent = File.ReadAllText(fileName);
+            BoardGameScenario createdScenario = JsonConvert.DeserializeObject<BoardGameScenario>(fileContent);
+            createdScenario.folderPath = Path.GetDirectoryName(fileName);
+            return createdScenario;
+        }
+
+        public string GetImageFolder()
+        {
+            return folderPath;
+        }
+    }
+
+    public class BoardGameScene
+    {
+        int currentDialogue;
+        public string Image { get; set; }
+        public string Dialogue { get; set; }
+    }
+}
